76c0368eea7d63b45325655ff8430e65a55e524f
wangqiang-h <948829164@qq.com>
2020-10-30 17:23:19 +0800

    Update 12306_login.py

--- 12306_login.py
+++ 12306_login.py
@@ -1,88 +1,112 @@
 import time
+import re
+import base64
+import json
+import sys
 from selenium.webdriver import ActionChains
 from get_code_position import get_position
 from browsermobproxy import Server
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import base64
-import re
-import json
-import sys
-import random
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
-server = Server(r'D:\browsermob-proxy-2.1.4\bin\browsermob-proxy.bat')
-server.start()
-proxy = server.create_proxy(params={'trustAllServers': 'true'})
-chrome_options = Options()
-chrome_options.add_argument('--proxy-server={0}'.format(proxy.proxy))
-# chrome_options.add_argument(
-#     "user-agent=User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36")
+BrowsermobPath = r'D:\browsermob-proxy-2.1.4\bin\browsermob-proxy.bat'
+Login12306Url = "https://kyfw.12306.cn/otn/resources/login.html"
+UserName = '*****@qq.com'
+PassWord = '*****'
 
-browser = webdriver.Chrome(chrome_options=chrome_options)
-proxy.new_har("new_har", options={'captureHeaders': True, 'captureContent': True})
 
-url = "https://kyfw.12306.cn/otn/resources/login.html"
-browser.get(url)
-browser.implicitly_wait(1)  # 静默等待最大5秒，保证页面加载完毕
-browser.maximize_window()  # 窗口最大化
+class Login12306:
+    def __init__(self):
+        self.server = Server(BrowsermobPath)
+        self.server.start()
+        self.proxy = self.server.create_proxy(params={'trustAllServers': 'true'})
+        self.proxy.new_har("new_har", options={'captureHeaders': True, 'captureContent': True})
 
-browser.find_elements_by_link_text("账号登录")[0].click()
+        options = Options()
+        options.add_argument('--no-sandbox')
+        options.add_argument('--proxy-server={0}'.format(self.proxy.proxy))
+        options.add_experimental_option('excludeSwitches',
+                                        ['enable-automation'])
 
-binary_raw_image = b''
-time.sleep(1)
+        self.browser = webdriver.Chrome(chrome_options=options)
+        self.browser_wait = WebDriverWait(self.browser, 10)
 
-result = proxy.har
-for entry in result['log']['entries']:
-    url = entry['request']['url']
-    content = entry['response']['content']
-    if '/passport/captcha/captcha-image64' in url:
-        print(url, content)
-        if 'text' not in content:
-            continue
-        search_result = re.search(r'{.*}', content["text"])
-        base64_raw_image = json.loads(search_result.group())["image"]
-        binary_raw_image = base64.b64decode(base64_raw_image)
-        break
+        try:
+            self.startlogin()
+            time.sleep(300)
+        finally:
+            self.close()
 
-if not binary_raw_image:
-    print('not find binary_raw_image')
-    sys.exit()
+    def startlogin(self):
+        self.browser.get(Login12306Url)
+        # 参考https://blog.csdn.net/weixin_44685869/article/details/105602629 不加过不了阿里的验证
+        script = 'Object.defineProperty(navigator,"webdriver",{get:() => false,});'
+        self.browser.execute_script(script)
+        self.browser.maximize_window()
 
-all_list = get_position(binary_raw_image)
-print("get parse", all_list)
-code_img_ele = browser.find_element_by_xpath('//*[@id="J-loginImg"]')
+        login_element = self.browser_wait.until(EC.presence_of_element_located((By.LINK_TEXT, "账号登录")))
+        login_element.click()
+        code_img_element = self.browser_wait.until(EC.presence_of_element_located((By.ID, "J-loginImg")))
+        binary_raw_image = b''
+        for _ in range(10):
+            binary_raw_image = self.getCaptchaImage()
+            if binary_raw_image:
+                break
+            time.sleep(0.1)
+        if not binary_raw_image:
+            print("can not capture data of captcha image")
+            self.close()
+            sys.exit(-1)
+        self.ProcessCaptcha(binary_raw_image, code_img_element)
+        self.ProcessSlideBlock()
 
-# 遍历位置列表,使用动作链对每一个列表元素对应的x，y指定的位置进行点击操作
-for l in all_list:
-    x = l[0]
-    y = l[1]
-    ActionChains(browser).move_to_element_with_offset(code_img_ele, x, y).click().perform()  # 动作链
-    time.sleep(0.2)
+    def getCaptchaImage(self):
+        # 抓包的方法比截图的方法更简单
+        binary_raw_image = b''
+        result = self.proxy.har
+        for entry in result['log']['entries']:
+            url = entry['request']['url']
+            content = entry['response']['content']
+            if '/passport/captcha/captcha-image64' in url and 'text' in content:
+                search_result = re.search(r'{.*}', content["text"])
+                base64_raw_image = json.loads(search_result.group())["image"]
+                binary_raw_image = base64.b64decode(base64_raw_image)
+                break
 
-# 输入用户名密码
-browser.find_element_by_id('J-userName').send_keys('948829164@qq.com')
-browser.find_element_by_id('J-password').send_keys('948829164wq')
-browser.find_elements_by_link_text("立即登录")[0].click()
+        return binary_raw_image
 
-time.sleep(600)
+    def ProcessCaptcha(self, binary_raw_image, code_img_element):
+        all_position_list = get_position(binary_raw_image)
+        print('get image postion %s ' % all_position_list)
 
-# 加入动作链
-div_tag = browser.find_element_by_xpath('//*[@id="nc_1_wrapper"]')
+        for x, y in all_position_list:
+            ActionChains(self.browser).move_to_element_with_offset(code_img_element, x, y).click().perform()
+            time.sleep(0.2)
 
-# 对div_tag进行滑动操作
-action = ActionChains(browser)  # 实例化一个动作对象
-action.click_and_hold(div_tag)  # 点击且长按不放
+        self.browser.find_element_by_id('J-userName').send_keys(UserName)
+        self.browser.find_element_by_id('J-password').send_keys(PassWord)
+        login_element = self.browser_wait.until(EC.presence_of_element_located((By.LINK_TEXT, "立即登录")))
+        login_element.click()
 
-for _ in range(10):
-    action.move_by_offset(34, 0)
-    action.pause(0.05)
+    def ProcessSlideBlock(self):
+        div_tag = self.browser_wait.until(EC.presence_of_element_located((By.ID, "nc_1_n1z")))
+        action = ActionChains(self.browser)
+        action.click_and_hold(div_tag)
+        # for _ in range(10):
+        #     action.move_by_offset(30, 0)
+        #     action.pause(0.1)
+        action.move_by_offset(300, 0)
+        action.perform()
+        action.release()
 
-action.perform()
+    def close(self):
+        self.proxy.close()
+        self.browser.quit()
+        self.server.stop()
 
-action.release()
 
-time.sleep(600)
-
-proxy.close()
-
-browser.quit()
+if __name__ == '__main__':
+    Login12306()
